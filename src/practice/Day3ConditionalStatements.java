9797fcd465648169839442687eefa903987c6082
dev-rm <54745323+dev-rm@users.noreply.github.com>
2021-02-27 08:36:16 +0530

    Add Day 3 solution

---
+++ src/practice/Day3ConditionalStatements.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.util.*;
+
+public class Day3ConditionalStatements {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        if(N%2 == 0){
+            if( N>= 2 && N<5)
+                System.out.println("Not Weird");
+            else if( N>=6 && N<=20)
+                System.out.println("Weird");
+            else if( N> 20)
+            System.out.println("Not Weird");
+        }
+        else{
+            System.out.println("Weird");
+        }
+    }
+}
